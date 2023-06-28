ebfaebe89114601eb9f0150a1b31fb04a006c697
Santiago <sagos0919@gmail.com>
2023-06-28 02:08:36 +0100

    FAQDropdown component is now animated and works as intended

--- src/components/sections/faq/FAQDropdown.js
+++ src/components/sections/faq/FAQDropdown.js
@@ -1,5 +1,5 @@
 import { ChevronDownIcon } from '@chakra-ui/icons';
-import { Flex, Text, useDisclosure } from '@chakra-ui/react';
+import { Collapse, Flex, Text } from '@chakra-ui/react';
 
 export default function FAQDropdown({
 	headerText,
@@ -8,31 +8,39 @@ export default function FAQDropdown({
 	onToggle,
 }) {
 	return (
-		<>
-			<Flex
-				onClick={onToggle}
-				w={'full'}
-				p={6}
-				bg={isOpen ? 'white.100' : 'white.50'}
-				roundedTop={'md'}
-			>
-				<ChevronDownIcon boxSize={5} color={'blue.200'} mr={6} />
-				<Text fontSize={'lg'}>{headerText}</Text>
+		<Flex
+			w={'full'}
+			p={6}
+			bg={isOpen ? 'white.100' : 'white.50'}
+			direction={'column'}
+			rounded={'md'}
+			onClick={onToggle}
+			cursor={'pointer'}
+		>
+			<Flex>
+				<ChevronDownIcon
+					boxSize={5}
+					color={'blue.200'}
+					mr={6}
+					transform={isOpen ? 'scaleY(-1)' : 'none'}
+					transition="transform 0.3s ease-in-out"
+				/>
+				<Text fontSize={'lg'} fontWeight={'medium'}>
+					{headerText}
+				</Text>
 			</Flex>
-			{isOpen && (
+			<Collapse in={isOpen} animateOpacity>
 				<Text
 					w={'full'}
-					pr={6}
-					pb={6}
-					pl={16}
+					pt={6}
+					pl={12}
 					alignSelf={'self-start'}
 					color={'grey'}
 					bg={'white.100'}
-					roundedBottom={'md'}
 				>
 					{bodyText}
 				</Text>
-			)}
-		</>
+			</Collapse>
+		</Flex>
 	);
 }
